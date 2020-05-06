1383a7e359224e330f5fe26afb4d67c1cfd19658
olawale micheal juwon <52759812+olawalejuwonm@users.noreply.github.com>
2020-05-06 17:34:49 +0100

    Update onlogin.js

--- onlogin.js
+++ onlogin.js
@@ -32,11 +32,11 @@ function getLocation() {
     }
   }
 
-function myMap() {
-    var mapProp = {
-        center:new google.maps.LatLng(51.508742,-0.120850),
-        zoom:5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
+//function myMap() {
+ //   var mapProp = {
+ //       center:new google.maps.LatLng(51.508742,-0.120850),
+ //       zoom:5,
+ //   };
+  //  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+ //   }
 
